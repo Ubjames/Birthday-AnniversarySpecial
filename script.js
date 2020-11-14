109eebc86b3636f8ca50a88af8a325f7802686f1
Ubong James <ubjames001@gmail.com>
2020-11-14 11:29:49 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let userId = document.querySelectorAll("input[type=text]");
+let post = document.querySelector("button[type=submit]");
+let ErrorMsg = document.querySelector("#Error-msg");
+let textArea = document.querySelector("textarea");
+
+
+// post.addEventListener('click', e =>{
+//     e.preventDefault()
+//     let userName;
+//     let userLocation;
+//     let userMessage;
+//     userName = userId[0].value;
+//     userLocation = userId[1].value;
+//     userMessage = textArea.value;
+//     console.log(userName + ' from ' + userLocation + ' sends: ' + userMessage)
+
+
+
+
+  
+// })
